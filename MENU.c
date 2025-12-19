3e61832afc5414fddd27743afb02a512c02f9b90
Wael-06 <youssefwael.work@gmail.com>
2025-12-19 14:10:42 +0200

    Update MENU.c and remove login2.c

--- MENU.c
+++ MENU.c
@@ -1,9 +1,12 @@
 #ifndef BANK_SYSTEM_H
 #define BANK_SYSTEM_H
+#ifndef LOGIN_H
+#define LOGIN_H
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "login.h"
 #define MAX_ACCOUNTS 1000
 void clear_input();//BUFFER 
 
@@ -18,66 +21,7 @@ int sizefile(char *fname)//byyoussef
     return size;
 }
 /*to get size of any file was planning in the login func. but didn't needed,but will useful as we go on*/
-int login()//byyoussef
-{
-    FILE *fptr = fopen("users.txt", "r");
-    if (!fptr)
-    {
-        return -1;
-    }
-
-    char file_username[50], file_password[10];
-    char input_username[50], input_password[10];
-    char line[100];
-
-    int attempts = 0;
-
-    while (attempts < 3)
-    {
-        printf("Enter Username: ");
-        fgets(input_username, sizeof(input_username), stdin);
-        input_username[strcspn(input_username, "\n")] = '\0';
-        
-        printf("Enter Password: ");
-        fgets(input_password, sizeof(input_password), stdin);
-        input_password[strcspn(input_password, "\n")] = '\0';
-
-        rewind(fptr);
-
-        int success = 0;
-
-        while (fgets(line, sizeof(line), fptr))
-        {
-            line[strcspn(line, "\n")] = 0;
-
-            sscanf(line, "%49s %9s", file_username, file_password);
-
-            if (strcmp(input_username, file_username) == 0 &&strcmp(input_password, file_password) == 0)
-            {
-                success = 1;
-                break;
-            }
-        }
-
-        if (success)
-        {
-            printf("You have logged in successfully\n");
-            fclose(fptr);
-            return 1;
-        }
-
-        attempts++;
-
-        if (attempts == 2)
-            printf("This is your last trial\n");
-        else if (attempts < 3)
-            printf("Incorrect, try again\n");
-    }
-
-    printf("Sorry, You failed login\n");
-    fclose(fptr);
-    return 0;
-}
+int login();//byyoussef
 /*The login function have a sscanf which (Dr.Saleh) said it isn't safe I will fix it later ,but other than that it is work is as a newly lubricated motor */
 /*return 1 if successful,0 if failed,-1 if couldn't open the file*/
 //------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -115,4 +59,4 @@ void add_account();
 void change_status();      
 
 #endif
-
+#endif
